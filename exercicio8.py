755655de5c70224136821692037190781a5c0bee
Sandro Assunção <150607293+sandroassuncao@users.noreply.github.com>
2023-11-28 07:25:53 -0300

    Add files via upload

---
+++ exercicio8.py
@@ -0,0 +1,10 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços?', a.isspace())
+print('É um número?', a.isnumeric())
+print('É alfabetico?', a.isalnum())
+print('É Alfanumerico?', a.isalpha())
+print('Está em maiuscula?',a.isupper())
+print('Está em minuscula?', a.islower())
+print('Éstá capitalizado?', a.capitalize())
+
