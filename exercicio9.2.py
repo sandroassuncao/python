1ec580c3abfdfccc0f3e3df1f0fc29fc64690132
Sandro Assunção <150607293+sandroassuncao@users.noreply.github.com>
2023-11-28 16:44:36 -0300

    Add files via upload

---
+++ exercicio9.2.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um numero:'))
+s = n + 1
+a = n - 1
+print('Analisando o valor   {}, seu sucessor é {} e o antecessor é {}'.format(n,s,a) )
---
+++ exercicio9.3.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um numero:'))
+print(' Analisando o valor {}, seu sucessor é {} e o antecessor é {}'.format(n,(n+1),(n-1)))
